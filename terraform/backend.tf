523b5986ac3da0375dbe057318802b7ece7def67
shaunak <shaunakondare44@gmail.com>
2026-04-02 12:20:27 +0530

    changed backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "aws-tf-backend-shaunak-21"
+    bucket         = "aws-tf-backend-shaunak-221"
     key            = "state/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "tfstate-lock"
